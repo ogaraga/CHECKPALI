57f0a5d3568136b5efc535b963194683cf1cfd9c
Ogaraga <124035624+ogaraga@users.noreply.github.com>
2023-07-10 22:32:05 +0100

    Add files via upload

--- recur.js
+++ recur.js
@@ -9,10 +9,10 @@ var pali_1 = "Wont lovers revolt now",
 for (i = 0; i < pali_1.length; i++) {
     var p = pali_1.charAt([i]).toLowerCase();
     var q = pali_1.charAt(pali_1.length - (i + 1)).toLowerCase();
-    (p === q) ? palindrome.textContent = (pali_1`: ${drome}`) : palindrome.textContent = (pali_1`: ${rome}`);
+    (p === q) ? palindrome.textContent = (`${pali_1}: ${drome}`) : palindrome.textContent = (`${pali_1}: ${rome}`);
 }
 for (j = 0; j < pali_2.length; j++) {
     let M = pali_2.charAt([j]).toLowerCase();
     let N = pali_2.charAt(pali_2.length - (j + 1)).toLowerCase();
-    { (N === M) ? notPalindrome.innerText = (pali_2` :${drome}`) : notPalindrome.innerText = (pali_2`: ${rome}`) };
+    { (N === M) ? notPalindrome.innerText = (`${pali_2} :${drome}`) : notPalindrome.innerText = (`${pali_2}: ${rome}`) };
 }
