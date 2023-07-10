5f251c4d2e667153bce00fefcbb185af85cf13f6
Ogaraga <124035624+ogaraga@users.noreply.github.com>
2023-07-10 22:29:14 +0100

    Add files via upload

--- recur.js
+++ recur.js
@@ -3,16 +3,16 @@ let palindrome = document.getElementById("palindrome"),
 
 var pali_1 = "Wont lovers revolt now",
     pali_2 = "RACECARS",
-    drome = "It's a palindrome",
-    rome = "It's not a palindrome",
+    drome = "Is a palindrome",
+    rome = "Is not a palindrome",
     i, j;
 for (i = 0; i < pali_1.length; i++) {
     var p = pali_1.charAt([i]).toLowerCase();
     var q = pali_1.charAt(pali_1.length - (i + 1)).toLowerCase();
-    (p === q) ? palindrome.textContent = (`${drome}`) : palindrome.textContent = (`${rome}`);
+    (p === q) ? palindrome.textContent = (pali_1`: ${drome}`) : palindrome.textContent = (pali_1`: ${rome}`);
 }
 for (j = 0; j < pali_2.length; j++) {
     let M = pali_2.charAt([j]).toLowerCase();
     let N = pali_2.charAt(pali_2.length - (j + 1)).toLowerCase();
-    { (N === M) ? notPalindrome.innerText = (`${drome}`) : notPalindrome.innerText = (`${rome}`) };
+    { (N === M) ? notPalindrome.innerText = (pali_2` :${drome}`) : notPalindrome.innerText = (pali_2`: ${rome}`) };
 }
